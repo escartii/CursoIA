d84231d4a5635bdb7ff30255a66c430e14e6626b
Alvaro Escarti <escartismx@gmail.com>
2025-08-05 16:23:51 +0200

    [] - Dia 1

---
+++ Dia1/dia1_ejercicio2.py
@@ -0,0 +1,22 @@
+# Definir variables diferentes tipos de datos
+
+entero = 10
+flotante = 3.14
+cadena = "IA"
+lista = [1, 2, 3]
+tupla = (4, 5, 6)
+diccionario = {
+    "nombre" : "Alvaro",
+    "rol" : "Programador"
+}
+booleano = True
+
+# Imprimir las variables
+print ( "Numero entero: ", entero)
+print ("Numero floatante: ", flotante)
+print ("Cadena de texto: ", cadena + " BootCamp")
+lista.append(4)
+print ("Lista: ", lista)
+print ("Tupla: ", tupla)
+print ("Diccionario: ", diccionario["nombre"])
+print ("Booleano: ", booleano)
