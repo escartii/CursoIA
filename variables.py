4e3f1b38d43123afb766915accc48d74ce483535
Alvaro Escarti <escartismx@gmail.com>
2025-08-05 16:13:00 +0200

    [] - Variables

---
+++ variables.py
@@ -0,0 +1,22 @@
+#Enteros y Flotantes
+edad = 24
+altura = 1.70
+
+# Cadenas de texto
+nombre = "Alvaro"
+apellido = nombre + " " + "Escartí"
+
+# Lista y Tupla
+numeros = [1,2,3,4,5]
+nombres = ("Alvaro", "Arbenita")
+coordenadas = (1.70, 2.00)
+
+# Diccionario
+persona = {
+    "nombre": "Alvaro",
+    "edad" : 24
+}
+
+# Booleanos
+estudiante = True
+trabajador = False
