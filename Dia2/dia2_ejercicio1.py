5ce69a8d1a0a06381eaf694bb9d87008a08eb4b8
Alvaro Escarti <escartismx@gmail.com>
2025-08-20 18:50:54 +0200

    Dia 2

---
+++ Dia2/dia2_ejercicio1.py
@@ -0,0 +1,20 @@
+# Ejemplo 1
+
+num = 10
+
+if num > 0:
+    print ("El numero es positivo")
+elif num == 0:
+    print ("El numero es 0")
+else:
+    print ("El numero es negativo")
+    
+# Ejemplo 2
+
+edad = 25
+
+if edad > 18:
+    if edad < 30:
+        print ("es un adulto joven")
+    else:
+        print ("adulto")
---
+++ Dia2/dia2_ejercicio2.py
@@ -0,0 +1,18 @@
+# Bucles
+
+#Recorrer una lista
+frutas = ["manzana", "platano", "cerezas"]
+for i in frutas:
+    print(i)
+
+#Recorrer una secuencia    
+for x in range(5):
+    print(x)
+    
+# Bucle While
+contador = 5
+while contador > 0:
+    print (contador)
+    contador = contador -1
+    
+print ("fuera del bucle")
